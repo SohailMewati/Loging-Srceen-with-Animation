8030d45b12c9abeeff0b8572a6adaca52f37adbb
Lenovo <sftprogrammer2022@gmail.com>
2023-04-20 13:31:56 +0530

    Login design with impressive animation

--- app/src/main/java/com/sohel/erplogin/HomeActivity.kt
+++ app/src/main/java/com/sohel/erplogin/HomeActivity.kt
@@ -2,10 +2,17 @@ package com.sohel.erplogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        getData()
+    }
+
+    fun getData(){
+        Toast.makeText(this, "You write new new in kotlin line file", Toast.LENGTH_SHORT).show()
     }
 }
